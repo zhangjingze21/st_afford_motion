decbe667fc98b9c3cee7340abbb4e5bc6e76d86a
zhangjingze <zhangjingze21@gmail.com>
2024-11-02 00:09:11 +0800

    add training code

--- scripts/mask_modeling/train_ddp.sh
+++ scripts/mask_modeling/train_ddp.sh
@@ -6,15 +6,15 @@
 #SBATCH --cpus-per-task=48
 #SBATCH --time 24:00:00
 
-EXP_NAME=${1:-"stfsq_ddp4"}
+EXP_NAME=${1:-"scene2motion"}
 
-HYDRA_FULL_ERROR=1 /mnt/lustre/home/jingze/anaconda3/envs/afford/bin/torchrun --nnodes=1 --nproc_per_node=4 --rdzv_backend=c10d --rdzv_endpoint=localhost:0 train_stfsq_ddp.py \
+HYDRA_FULL_ERROR=1 /mnt/lustre/home/jingze/anaconda3/envs/afford/bin/torchrun --nnodes=1 --nproc_per_node=4 --rdzv_backend=c10d --rdzv_endpoint=localhost:0 train_mask_trans_ddp.py \
             hydra/job_logging=none hydra/hydra_logging=none \
             exp_name=${EXP_NAME} \
-            output_dir=outputs/stage1/ \
+            output_dir=outputs/stage2 \
             platform=TensorBoard \
-            task=motion_quantizer \
-            task.train.batch_size=128 \
+            task=mask_modeling \
+            task.train.batch_size=64 \
             task.train.phase=train \
             task.dataset.sigma=0.8 \
-            model=sthvqvae \
+            model=mask_transformer \
---
+++ scripts/sthvqvae/train_ddp.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#SBATCH --partition=SC-A800
+#SBATCH --job-name="humanise"
+#SBATCH --gres=gpu:4
+#SBATCH --qos=plus
+#SBATCH --cpus-per-task=48
+#SBATCH --time 24:00:00
+
+EXP_NAME=${1:-"stfsq_ddp4_lvl0.1_vq0.01"}
+
+HYDRA_FULL_ERROR=1 /mnt/lustre/home/jingze/anaconda3/envs/afford/bin/torchrun --nnodes=1 --nproc_per_node=4 --rdzv_backend=c10d --rdzv_endpoint=localhost:0 train_stfsq_ddp.py \
+            hydra/job_logging=none hydra/hydra_logging=none \
+            exp_name=${EXP_NAME} \
+            output_dir=outputs/stage1/ \
+            platform=TensorBoard \
+            task=motion_quantizer \
+            task.train.batch_size=64 \
+            task.train.phase=train \
+            task.dataset.sigma=0.8 \
+            model=sthvqvae \
