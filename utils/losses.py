95f8ac031423e8c30dbc6b8db2a79ae4b881b377
zhangjingze <zhangjingze21@gmail.com>
2024-11-02 00:10:00 +0800

    add mask modeling

--- utils/losses.py
+++ utils/losses.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
 
 class MultiScaleReConsLossWithMask(nn.Module):
     def __init__(self, recons_loss, nb_joints):
@@ -38,3 +40,37 @@ def calculate_mpjpe(gt_joints, pred_joints):
     mpjpe_seq = mpjpe.mean(-1) # num_poses
 
     return mpjpe_seq
+
+def cal_performance(pred, labels, ignore_index=None, smoothing=0., tk=1):
+    loss = cal_loss(pred, labels, ignore_index, smoothing=smoothing)
+    # pred_id = torch.argmax(pred, dim=1)
+    # mask = labels.ne(ignore_index)
+    # n_correct = pred_id.eq(labels).masked_select(mask)
+    # acc = torch.mean(n_correct.float()).item()
+    pred_id_k = torch.topk(pred, k=tk, dim=1).indices
+    pred_id = pred_id_k[:, 0]
+    mask = labels.ne(ignore_index)
+    n_correct = (pred_id_k == labels.unsqueeze(1)).any(dim=1).masked_select(mask)
+    acc = torch.mean(n_correct.float()).item()
+
+    return loss, pred_id, acc
+
+def cal_loss(pred, labels, ignore_index=None, smoothing=0.):
+    '''Calculate cross entropy loss, apply label smoothing if needed.'''
+    # print(pred.shape, labels.shape) #torch.Size([64, 1028, 55]) torch.Size([64, 55])
+    # print(pred.shape, labels.shape) # torch.Size([24, 210, 1025]) torch.Size([24, 210])
+    if smoothing:
+        space = 2
+        n_class = pred.size(1)
+        mask = labels.ne(ignore_index)
+        one_hot = rearrange(F.one_hot(labels, n_class + space), 'a ... b -> a b ...')[:, :n_class]
+        # one_hot = torch.zeros_like(pred).scatter(1, labels.unsqueeze(1), 1)
+        sm_one_hot = one_hot * (1 - smoothing) + (1 - one_hot) * smoothing / (n_class - 1)
+        neg_log_prb = -F.log_softmax(pred, dim=1)
+        loss = (sm_one_hot * neg_log_prb).sum(dim=1)
+        # loss = F.cross_entropy(pred, sm_one_hot, reduction='none')
+        loss = torch.mean(loss.masked_select(mask))
+    else:
+        loss = F.cross_entropy(pred, labels, ignore_index=ignore_index)
+
+    return loss
--- utils/training.py
+++ utils/training.py
@@ -10,6 +10,164 @@ from diffusion.resample import uniform_sampling
 
 from utils.losses import MultiScaleReConsLossWithMask, calculate_mpjpe
 
+class MaskTransformerTrainLoop:
+    def __init__(self, *, cfg, model, dataloader, **kwargs) -> None:
+        self.model = model
+        self.dataloader = dataloader
+        self.cfg = cfg
+
+        self.lr = cfg.lr
+        self.max_steps = cfg.max_steps
+        self.max_epochs = cfg.max_steps // len(self.dataloader) + 1
+        self.log_every_step = cfg.log_every_step
+        self.save_every_step = cfg.save_every_step
+
+        self.resume_checkpoint = cfg.resume_ckpt
+        self.weight_decay = cfg.weight_decay
+        self.lr_anneal_steps = cfg.lr_anneal_steps
+        
+        self.device = kwargs['device'] if 'device' in kwargs else 'cpu'
+        self.save_dir = kwargs['save_dir'] if 'save_dir' in kwargs else '/tmp'
+        self.gpu = kwargs['gpu'] if 'gpu' in kwargs else 0
+        self.is_distributed = kwargs['is_distributed'] if 'is_distributed' in kwargs else False
+
+        self.step = 1
+        self.resume_step = self._load_and_sync_parameters()
+
+        ## set optimizer
+        params = []
+        nparams = []
+        for n, p in model.named_parameters():
+            if p.requires_grad:
+                params.append(p)
+                nparams.append(p.nelement())
+                if self.gpu == 0:
+                    logger.info(f'Add {n} {p.shape} for optimization.')
+        if self.gpu == 0:
+            logger.info(f'{len(params)} parameters for optimization.')
+            logger.info(f'Total model size is {(sum(nparams) / 1e6):.2f} M.')
+        
+        self.optimizer = torch.optim.AdamW(
+            params, lr=self.lr, weight_decay=self.weight_decay
+        )
+        if self.resume_step:
+            self.step = self.resume_step + 1
+            self._load_optimizer_state()
+        
+    def _load_and_sync_parameters(self):
+        """ Load model from checkpoint if provided for resuming. """
+        def parse_resume_step_from_filename(path):
+            filename = os.path.basename(path)
+            return int(filename.replace('.pt', '').replace('model', ''))
+        
+        resume_step = 0
+        if self.resume_checkpoint:
+            resume_step = parse_resume_step_from_filename(self.resume_checkpoint)
+            load_ckpt(self.model, self.resume_checkpoint)
+            if self.gpu == 0:
+                logger.info(f"Loading model from checkpoint: {self.resume_checkpoint}...")
+            
+        return resume_step
+        
+    def _load_optimizer_state(self):
+        """ Load optimizer state from checkpoint if provided for resuming. """
+        opt_checkpoint = os.path.join(
+            os.path.dirname(self.resume_checkpoint),
+            "opt.pt"
+        )
+        
+        if os.path.exists(opt_checkpoint):
+            self.optimizer.load_state_dict(
+                torch.load(opt_checkpoint)
+            )
+            if self.gpu == 0:
+                logger.info(f"Loading optimizer state from checkpoint: {opt_checkpoint}...")
+
+    def _anneal_lr(self):
+        if not self.lr_anneal_steps:
+            return
+        frac_done = (self.step + self.resume_step) / self.lr_anneal_steps
+        lr = self.lr * (1 - frac_done)
+        for param_group in self.optimizer.param_groups:
+            param_group["lr"] = lr
+
+    def _save(self):
+        """ Save model and optimizer state. """
+        saved_state_dict = {}
+        model_state_dict = self.model.state_dict()
+        for key in model_state_dict:
+            if 'scene_model' in key or 'clip_model' in key or 'text_model' in key or 'bert_model' in key:
+                continue
+
+            saved_state_dict[key] = model_state_dict[key]
+        
+        with open(os.path.join(self.save_dir, f"model{self.step:06d}.pt"), "wb") as f:
+            torch.save(saved_state_dict, f)
+
+        with open(os.path.join(self.save_dir, f"opt.pt"), "wb") as f: # only save the last optimizer state for saving space
+            torch.save(self.optimizer.state_dict(), f)
+        
+        if self.gpu == 0:
+            logger.info(f'Model saved! [Step: {self.step:06d}]')
+    
+    def _freeze_scene_model_batchnorm(self):
+        """ Freeze batchnorm in scene model if the model has scene model. """
+        if hasattr(self.model, 'scene_model') and self.model.freeze_scene_model :
+            for m in self.model.scene_model.modules():
+                if isinstance(m, nn.BatchNorm1d) or isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm3d):
+                    m.eval()
+
+    def run_loop(self):
+        for epoch in range(1, self.max_epochs + 1):
+            self.model.train()
+            self._freeze_scene_model_batchnorm() # freeze batchnorm in scene model if the model has scene model
+            if self.is_distributed:
+                self.dataloader.sampler.set_epoch(epoch)
+            for it, data in enumerate(self.dataloader): 
+                x = data['x'].to(self.device)
+
+                x_kwargs = {}
+                if 'x_mask' in data:
+                    x_kwargs['x_mask'] = data['x_mask'].to(self.device)
+                
+                for key in data:
+                    if key.startswith('c_') :
+                        if torch.is_tensor(data[key]):
+                            x_kwargs[key] = data[key].to(self.device)
+                        else:
+                            x_kwargs[key] = data[key]
+
+                ## one step optimization
+                self.optimizer.zero_grad()
+
+                loss, acc, _ = self.model(x, **x_kwargs)
+                
+                loss.backward()
+
+                self.optimizer.step()
+                self._anneal_lr()
+                
+                ## log and save
+                ## log with loguru, plot with Board
+                if self.gpu == 0 and self.step % self.log_every_step == 0:
+                    ## log with loguru
+                    logger.info(
+                        f"[TRAIN] ==> Epoch: {epoch:3d} | Iter: {it+1:5d} | Step: {self.step:7d} | Loss: {loss:8.5f} | Acc: {acc:8.5f}"
+                    )
+
+                    ## plot with Board
+                    write_dict = {'step': self.step, 'train/epoch': epoch, 'train/loss': loss.item(), 'train/acc': acc.item()}
+                    Board().write(write_dict)
+
+                if self.gpu == 0 and self.step % self.save_every_step == 0:
+                    ## save model
+                    self._save()
+                
+                ## update step and check max steps
+                self.step += 1
+                if self.step > self.max_steps:
+                    return
+
 class STFSQTrainLoop:
     def __init__(self, *, cfg, model, dataloader, test_dataset, test_dataloader, **kwargs) -> None:
         self.model = model
